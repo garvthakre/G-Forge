52c39426a998fd101f14b7cb4c8129578b5da71d
garv thakre <garvthakre0@gmail.com>
2026-01-21 12:56:44 +0530

    feat:redesign header

--- ui/app/components/header.tsx
+++ ui/app/components/header.tsx
@@ -1,16 +1,38 @@
-import { NextPage } from 'next'
+import React from "react";
 
-interface Props {}
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center h-16 bg-gray-900 text-gray-200 px-5 border-b border-gray-800">
+      <div className="flex items-center">
+        <span className="text-2xl mr-2">{`{}`}</span>
+        <span className="font-bold text-xl mr-1">GARV</span>
+    
+      </div>
+      <nav className="flex-grow mx-10">
+        <ul className="flex space-x-6">
+          <li className="uppercase cursor-pointer hover:text-blue-400">HOME</li>
+          <li className="uppercase cursor-pointer hover:text-blue-400">
+            PROJECTS
+          </li>
+          <li className="uppercase cursor-pointer hover:text-blue-400">
+            EXPERIENCE
+          </li>
+          <li className="uppercase cursor-pointer hover:text-blue-400">
+            API LAB
+          </li>
+        </ul>
+      </nav>
+      <div className="flex items-center">
+        <span className="flex items-center mr-4">
+          <span className="w-2.5 h-2.5 rounded-full bg-green-500 mr-1"></span>{" "}
+          ONLINE
+        </span>
+        <button className="border border-blue-400 text-blue-400 px-2 py-1 hover:bg-blue-500 hover:text-white">
+          Authorize
+        </button>
+      </div>
+    </header>
+  );
+};
 
-const Header: NextPage<Props> = ({}) => {
-  return <div>
-    <div className='bg-blue-300 flex justify-between p-4 flex-row flex'>
-    <div className='bg-red-300'>Garv Thakre</div>
-    <div className='bg-green-300'>Skills</div>
-    <div className='bg-yellow-300'>Projects</div>
-    <div className='bg-purple-300'>Experience</div>
-    </div>
-  </div>
-}
-
-export default Header
+export default Header;
